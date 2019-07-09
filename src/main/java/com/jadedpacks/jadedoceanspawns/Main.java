92432d0eb7ef27ea81856066ccebb88f877afbc3
Kris <cblair91@gmail.com>
2019-07-09 02:07:00 +0100

    Now with 100% more spawns

--- src/main/java/com/jadedpacks/jadedoceanspawns/Main.java
+++ src/main/java/com/jadedpacks/jadedoceanspawns/Main.java
@@ -8,37 +8,41 @@ import net.minecraft.entity.passive.*;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.SpawnListEntry;
 import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeDictionary.Type;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @Mod(modid = "jadedoceanspawns", name = "JadedOceanSpawns", version = "@VERSION@")
 public class Main {
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		for(final BiomeGenBase biome : BiomeDictionary.getBiomesForType(Type.WATER)) {
-			biome.enableRain = true;
-			biome.getSpawnableList(EnumCreatureType.creature).addAll(Arrays.asList(
-				new SpawnListEntry(EntitySheep.class, 8, 1, 4),
-				new SpawnListEntry(EntityCow.class, 8, 1, 4),
-				new SpawnListEntry(EntityChicken.class, 8, 1, 4),
-				new SpawnListEntry(EntityPig.class, 4, 1, 4)
-			));
-			biome.getSpawnableList(EnumCreatureType.monster).addAll(Arrays.asList(
-				new SpawnListEntry(EntitySkeleton.class, 8, 1, 2),
-				new SpawnListEntry(EntityZombie.class, 4, 1, 2),
-				new SpawnListEntry(EntitySpider.class, 6, 1, 2),
-				new SpawnListEntry(EntityWitch.class, 2, 1, 2),
-				new SpawnListEntry(EntityCreeper.class, 2, 1, 2),
-				new SpawnListEntry(EntityEnderman.class, 3, 1, 2)
-			));
-			biome.getSpawnableList(EnumCreatureType.waterCreature).add(new SpawnListEntry(EntitySquid.class, 8, 1, 2));
-			try {
-				biome.getSpawnableList(EnumCreatureType.monster).add(new SpawnListEntry(Class.forName("thaumcraft.common.entities.monster.EntityBrainyZombie"), 4, 1, 4));
-			} catch(Exception ignored) {}
-			try {
-				biome.getSpawnableList(EnumCreatureType.monster).add(new SpawnListEntry(Class.forName("ganymedes01.headcrumbs.entity.EntityCelebrity"), 2, 1, 2));
-			} catch(Exception ignored) {}
+		for(ArrayList<BiomeGenBase> biomes : BiomeDictionary.typeInfoList) {
+			for(final BiomeGenBase biome : biomes) {
+				biome.enableRain = true;
+				biome.getSpawnableList(EnumCreatureType.creature).addAll(Arrays.asList(
+					new SpawnListEntry(EntitySheep.class, 8, 1, 4),
+					new SpawnListEntry(EntityCow.class, 8, 1, 4),
+					new SpawnListEntry(EntityChicken.class, 8, 1, 4),
+					new SpawnListEntry(EntityPig.class, 4, 1, 4)
+				));
+				biome.getSpawnableList(EnumCreatureType.monster).addAll(Arrays.asList(
+					new SpawnListEntry(EntitySkeleton.class, 8, 1, 2),
+					new SpawnListEntry(EntityZombie.class, 4, 1, 2),
+					new SpawnListEntry(EntitySpider.class, 6, 1, 2),
+					new SpawnListEntry(EntityWitch.class, 2, 1, 2),
+					new SpawnListEntry(EntityCreeper.class, 2, 1, 2),
+					new SpawnListEntry(EntityEnderman.class, 3, 1, 2)
+				));
+				biome.getSpawnableList(EnumCreatureType.waterCreature).add(new SpawnListEntry(EntitySquid.class, 8, 1, 2));
+				try {
+					biome.getSpawnableList(EnumCreatureType.monster).add(new SpawnListEntry(Class.forName("thaumcraft.common.entities.monster.EntityBrainyZombie"), 4, 1, 4));
+				} catch(Exception ignored) {
+				}
+				try {
+					biome.getSpawnableList(EnumCreatureType.monster).add(new SpawnListEntry(Class.forName("ganymedes01.headcrumbs.entity.EntityCelebrity"), 2, 1, 2));
+				} catch(Exception ignored) {
+				}
+			}
 		}
 	}
 }
